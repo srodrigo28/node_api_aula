7363cba0a32d9737d5d74cc7b852fab5f76b39ca
srodrigo28 <rodrigoexer1@gmail.com>
2022-03-12 10:55:14 -0300

    Rotas Get iniciais criadas

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const res = require('express/lib/response')
 const app = express()
 app.listen('3000')
 
+app.route('/')     .get((req,res) => res.send('Hello'))
+app.route('/login').get((req,res) => res.send("Login"))
+app.route('/cadastro').get((req,res) => res.send("Cadastro"))
